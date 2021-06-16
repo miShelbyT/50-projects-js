5dee5479e17c51e7dac8713e258754e4b7996af0
miShelbyT <mi.shelbyrose@gmail.com>
2021-06-16 13:28:06 -0400

    added pagination to movie db and temporarily deleting API key for GH purposes

--- day_17_movie_app/script.js
+++ day_17_movie_app/script.js
@@ -1,8 +1,21 @@
-
-
 const form = document.getElementById('form')
 const search = document.getElementById('search')
 const main = document.getElementById('main')
+const prev = document.getElementById('prev')
+const next = document.getElementById('next')
+let page = '1'
+let numPage = Number(page)
+
+// clearly the page defaults to 1 implicitly in the endpoint
+
+
+
+const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
+
+const SEARCH_API =
+  'https://api.themoviedb.org/3/search/movie?api_key=404ed1ee832cf638edc0b474acb88169&query="'
+
+
 
 async function getMovies(url) {
   const res = await fetch(url)
@@ -42,6 +55,19 @@ form.addEventListener('submit', (e) => {
     getMovies(`${SEARCH_API}${searchTerm}`)
     search.value = ''
   } else window.location.reload()
+})
+
+prev.addEventListener('click', () => {
+  if(numPage > 1) numPage -= 1
+  else numPage = 1
+  page = numPage.toString()
+  getMovies(`${API_URL}${page}`)
+})
+
+next.addEventListener('click', () => {
+  numPage += 1
+  page = numPage.toString()
+  getMovies(`${API_URL}${page}`)
 })
 
 getMovies(API_URL)
