9170e69354116bc0b7a3dd2d0f1a02676547eca5
miShelbyT <mi.shelbyrose@gmail.com>
2021-08-18 16:28:30 -0400

    hid movie API key and pushing drawing app

--- day_17_movie_app/script.js
+++ day_17_movie_app/script.js
@@ -1,3 +1,5 @@
+import API_KEY from './apikey.js'
+
 const form = document.getElementById('form')
 const search = document.getElementById('search')
 const main = document.getElementById('main')
@@ -6,8 +8,16 @@ const next = document.getElementById('next')
 let page = '1'
 let numPage = Number(page)
 
-// pagination works! still need to figure out how to disable prev button when i get to page 1. prev.disabled shows up in Elements tab but doesn't actually disable the button 👀
+// pagination works! still need to finesse how to disable prev button when i get to page 1 and grey it out, and then reverse the process when page is > 1.
 
+let API_URL =
+`https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=${API_KEY}&page=`
+
+
+const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
+
+const SEARCH_API =
+  `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query="`
 
 
 
