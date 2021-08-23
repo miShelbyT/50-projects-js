3f60578f04e8952db158ccfddad5d8e197457da1
miShelbyT <mi.shelbyrose@gmail.com>
2021-08-23 10:53:05 -0400

    finished github project, super fun

--- day_28_github_profiles/script.js
+++ day_28_github_profiles/script.js
@@ -1,32 +1,37 @@
 const APIURL = 'https://api.github.com/users/'
 
 const main = document.getElementById('main')
-const form = document.getElementById("form")
-const search = document.getElementById("search")
+const form = document.getElementById('form')
+const search = document.getElementById('search')
 
 form.addEventListener('submit', (e) => {
   e.preventDefault()
   const user = search.value
-  if(user){
+  if (user) {
     getUser(user)
+    getRepos(user)
     form.reset()
   }
 })
 
 async function getUser(username) {
-  try{
+  try {
     const { data } = await axios(APIURL + username)
     // don't need to parse the JSON with axios
-    if(data.name == null) renderErrorCard('ooops. no profile with this user name. please try again.')
-    else createUserCard(data)
-  } catch(error) {
-    console.log(error.response.request.status)
-    if(error.response.request.status == 404) renderErrorCard('ooops. no profile with this user name. please try again.')
+    if (data.name == null)
+      renderErrorCard(
+        'ooops. no profile with this user name. please try again.'
+      )
+    else renderUserCard(data)
+  } catch (error) {
+    if (error.response.request.status == 404)
+      renderErrorCard(
+        'ooops. no profile with this user name. please try again.'
+      )
   }
-  
 }
 
-function createUserCard(user) {
+function renderUserCard(user) {
   const cardHTML = `<div class="card">
   <div>
     <img
@@ -50,14 +55,35 @@ function createUserCard(user) {
   </div>
 </div>`
 
-main.innerHTML = cardHTML
+  main.innerHTML = cardHTML
 }
 
-function renderErrorCard(message){
+async function getRepos(user) {
+  try {
+    const { data } = await axios(APIURL + user + '/repos?sort=created')
+    addReposToCard(data)
+  } catch (error) {
+    renderErrorCard("Problem fetching repos")
+  }
+}
+
+function addReposToCard(data){
+  const repoList = document.getElementById('repos')
+  for(let i = 0; i < 10; i++){
+    let repo = document.createElement('a')
+    repo.classList.add('repo')
+    repo.innerText = data[i].name
+    repo.href = data[i].html_url
+    repo.target = "_blank"
+    repoList.append(repo)
+  }
+  
+}
+
+function renderErrorCard(message) {
   const cardHTML = `
   <div class="card">
   <h1>${message}</h1>
   </div>`
   main.innerHTML = cardHTML
 }
-
