4a81a9ecfeb705a3e5ce9b90c24f48df59078b95
miShelbyT <mi.shelbyrose@gmail.com>
2021-09-12 12:55:38 -0400

    fixed regex in prev project and completed user fetch and filter project

--- day_39_password_strength/script.js
+++ day_39_password_strength/script.js
@@ -5,11 +5,11 @@ const background = document.getElementById('background')
 password.addEventListener('input', (e) => {
   const input = e.target.value
   const length = input.length
-  // const regex = new RegExp("^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$")
+  const regex = new RegExp("^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$")
   
-  // if(regex.test(length) == false){
+  if(regex.test(input) == true){
     const blurVal = 20 - length * 2
     background.style.filter = `blur(${blurVal}px)`
-  // }
+  }
   
 })
